a0042bff9e9c43cba2d4c0c9ebc8d515fa5995d6
David Chin <dlcmhd@me.com>
2021-07-23 12:05:20 +0800

    feature

--- theme.config.js
+++ theme.config.js
@@ -1,6 +1,6 @@
 export default {
-  repository: 'https://github.com/shuding/nextra',
-  titleSuffix: ' – Nextra',
+  repository: 'https://github.com/dlcmh/lnl-nextra-demo',
+  titleSuffix: ' | LnL',
   logo: (
     <>
       <span className="mr-2 font-extrabold hidden md:inline">Nextra</span>
